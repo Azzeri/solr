6d62af5c358875ebc038922b873dd0aa96d79f8d
Azzeri <azmario2698@gmail.com>
2023-05-08 20:32:25 +0200

    breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SolariumController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/search');
+    return view('/search');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/search', [SolariumController::class, 'index']);
 Route::get('/extract', [SolariumController::class, 'extract']);
@@ -24,4 +37,3 @@ Route::post('/crawl', [SolariumController::class, 'crawl']);
 Route::post('/search', [SolariumController::class, 'search']);
 Route::get('/cleanDocuments', [SolariumController::class, 'cleanDocuments']);
 Route::get('/cleanDatabase', [SolariumController::class, 'cleanDatabase']);
-
